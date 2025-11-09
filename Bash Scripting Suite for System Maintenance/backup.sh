ca16a993fa12e151e442b3555f2d89f767e47663
Ashutosh Sahoo <ashutoshsahoo8780@gmail.com>
2025-11-09 14:09:55 +0530

    Create backup.sh

---
+++ Bash Scripting Suite for System Maintenance/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SOURCE_DIR="/home/$USER/Documents"
+BACKUP_DIR="/home/$USER/system_backups"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
+
+mkdir -p "$BACKUP_DIR"
+tar -czf "$BACKUP_DIR/backup_$TIMESTAMP.tar.gz" "$SOURCE_DIR"
+
+echo "Backup completed successfully at $TIMESTAMP"
+echo "Backup File: $BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
